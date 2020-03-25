45d4fd934f4b532e1bd9eb135583daa3d0fc8f23
Allan <allanramirez.edutec@gmail.com>
2020-03-25 17:39:28 -0600

    agrego archivo javascript el cual llevara las animaciones

---
+++ js.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.header').mouseover(function() {
+        $('#titulo').animate({ fontSize: '26px' });
+    });
+
+    $('ul').mouseover(function() {
+        $('li').animate({
+            fontSize: "30px"
+        }, 1500);
+    });
+    $('.header').mouseover(function() {
+        $('h3').animate({ fontSize: '30px', opacity: '0.1' });
+    });
+    $('ul').mouseover(function() {
+        $('li').animate({
+            width: '160px',
+            fontSize: '20px'
+        }, 1500);
+    });
+    $('.responsive').mouseover(function() {
+        $('img').animate({
+            width: '160px'
+        }, 1500);
+        $('img').animate({
+            width: '600px'
+        });
+        $('img').animate({
+            width: '200px'
+        });
+    });
+})
